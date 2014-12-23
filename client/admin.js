aaf64258561dbd0624c1f5c2a426e78c44fdb2c4
adamwbowman <adamwbowman@gmail.com>
2014-12-23 13:29:10 -0600

    - admin for clients

--- client/admin.js
+++ client/admin.js
@@ -6,22 +6,30 @@
 // Helpers 
 Template.admin.helpers({
 	client: function () {
-		return Clients.find({}).fetch();
+		return Clients.find({}, {sort: {name: 1}}).fetch();
 	}
 });
 
 // Events
 Template.admin.events({
-	'click .addClient': function (evt) {
-		var clientName = $('.addClientInput').val();
+	'click .addClient': function (evt, template) {
+		var clientName = template.find('.addClientInput').value;
 		if (clientName.length > 0) {
 			Clients.insert({name: clientName});
-			$('.addClientInput').val('');
+			template.find('.addClientInput').value = '';
+		}
+	},
+	'keypress input.addClientInput': function (evt, template) {
+		if (evt.which === 13) {
+			var clientName = template.find('.addClientInput').value;
+			if (clientName.length > 0) {
+				Clients.insert({name: clientName});
+				template.find('.addClientInput').value = '';
+			}
 		}
 	},
 	'click .currentClient': function () {
 		Session.set('currentClient', this._id);
-		Session.set('currentClientName', this.name);
 	},
 	'click .deleteClient': function () {
 		Clients.remove({_id: this._id});
