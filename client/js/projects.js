f660bd65d61adf8fd971e909a5b94b2bed47245d
adamwbowman <adamwbowman@gmail.com>
2014-12-24 22:33:58 -0600

    - products typeahead

--- client/js/projects.js
+++ client/js/projects.js
@@ -10,14 +10,19 @@ Template.projects.helpers({
 		return Projects.find({}).fetch();
 	},
 	clients: function () {
-		var xxx = Clients.find({}).fetch();
-		var zzz = _.chain(xxx).pluck('name').value();
-		return JSON.stringify(zzz);
+		var clientColl = Clients.find({}).fetch();
+		var namePluck = _.chain(clientColl).pluck('name').value();
+		return JSON.stringify(namePluck);
 	},
 	locations: function () {
-		var xxx = Locations.find({}).fetch();
-		var zzz = _.chain(xxx).pluck('name').value();
-		return JSON.stringify(zzz);
+		var locationColl = Locations.find({}).fetch();
+		var namePluck = _.chain(locationColl).pluck('name').value();
+		return JSON.stringify(namePluck);
+	},
+	products: function () {
+		var productColl = Products.find({}).fetch();
+		var namePluck = _.chain(productColl).pluck('name').value();
+		return JSON.stringify(namePluck);
 	},
 });
 
@@ -26,8 +31,12 @@ Template.projects.events({
 	'click .submit': function (evt, template) {
 		Projects.insert({
 			name: template.find('#name').value,
+			client: template.find('#client').value,
+			location: template.find('#location').value,
 		});
 		template.find('#name').value = '';
+		template.find('#client').value = '';
+		template.find('#location').value = '';
 	},
 	'click .icon-trash': function (evt, template) {
 		Projects.remove({_id: this._id});
