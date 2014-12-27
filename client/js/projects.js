7ecff3a8d690a5d5b6fbd7dc615db273642af731
adamwbowman <adamwbowman@gmail.com>
2014-12-27 11:30:37 -0600

    - histories for projects

--- client/js/projects.js
+++ client/js/projects.js
@@ -29,7 +29,7 @@ Template.projects.helpers({
 // Events
 Template.projects.events({
 	'click .submit': function (evt, template) {
-		Projects.insert({
+		var projectId = Projects.insert({
 			name: template.find('#name').value,
 			client: template.find('#client').value,
 			EMEA: $('#EMEA').prop('checked'),
@@ -37,8 +37,23 @@ Template.projects.events({
 			Americas: $('#Americas').prop('checked'),
 			location: template.find('#location').value,
 			product: template.find('#product').value,
+			createdBy: Meteor.userId(),
+			createdByEmail: getUserEmail(),
 			date: new Date,
-		});
+		}, projectId);
+		Histories.insert({
+			projectId: projectId,
+			name: template.find('#name').value,
+			client: template.find('#client').value,
+			EMEA: $('#EMEA').prop('checked'),
+			APAC: $('#APAC').prop('checked'),
+			Americas: $('#Americas').prop('checked'),
+			location: template.find('#location').value,
+			product: template.find('#product').value,
+			createdBy: Meteor.userId(),
+			createdByEmail: getUserEmail(),
+			date: new Date,			
+		}); 
 		template.find('#name').value = '';
 		template.find('#client').value = '';
 		$('#EMEA').removeAttr('checked');
@@ -46,7 +61,7 @@ Template.projects.events({
 		$('#Americas').removeAttr('checked');
 		template.find('#location').value = '';
 		template.find('#product').value = '';
-	},
+	}, 
 	'click .icon-trash': function (evt, template) {
 		Projects.remove({_id: this._id});
 	}
@@ -58,3 +73,22 @@ Template.projects.events({
 Handlebars.registerHelper("formatStandardDate", function(date) {
 	return moment(date).format("MM/DD/YYYY");
 });
+
+Handlebars.registerHelper("listHistory", function(id) {
+	console.log(id);
+	return Histories.find({projectId: id}).fetch();
+});
+
+
+//////////////////////////////////////////////////////////////////////////////////
+// Methods...
+
+// Using their Meteor userId, find their email address
+var getUserEmail = function (item) {
+	if (Meteor.user()) {
+		var user = Meteor.user();
+		return user.emails[0].address;
+	} else {
+		return 'anon'
+	}
+};
