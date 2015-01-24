b986d2afff083ac2009f78596b71aff5513c38d3
adamwbowman <adamwbowman@gmail.com>
2015-01-24 13:01:57 -0600

    - added client add, edit, delete

---
+++ client/js/clients.js
@@ -0,0 +1,169 @@
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* project.js
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+Session.setDefault('create', true);
+Session.setDefault('edit', false);
+Session.setDefault('currentClient', null);
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* Helpers
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */ 
+Template.clients.helpers({
+	client: function () {
+		return Clients.find({}, {sort: {'name': 1}}).fetch();
+	},
+	locations: function () {
+		var locationColl = Locations.find({}).fetch();
+		var namePluck = _.chain(locationColl).pluck('name').value();
+		return JSON.stringify(namePluck);
+	},
+	editClient: function () {
+		return Clients.find({_id: Session.get('currentClient')}).fetch();
+	},
+	selectedProject: function () {
+		return Session.equals('currentClient', this._id) ? 'selected' : '';
+	},
+	isSV: function () {
+		// var thisProject = Projects.find({_id: this._id}).fetch();
+		// var project = _.chain(thisProject).pluck('product').value();
+		// if (project == 'Singleview') {
+		// 	return true;
+		// };
+	},
+	isWBMS: function () {
+		// var thisProject = Projects.find({_id: this._id}).fetch();
+		// var project = _.chain(thisProject).pluck('product').value();
+		// if (project == 'WBMS') {
+		// 	return true;
+		// };
+	},
+	isTSM: function () {
+		// var thisProject = Projects.find({_id: this._id}).fetch();
+		// var project = _.chain(thisProject).pluck('product').value();
+		// if (project == 'TSM') {
+		// 	return true;
+		// };
+	},
+	pageCreateState: function () {
+		return Session.get('create');
+	},
+	pageEditState: function () {
+		return Session.get('edit');
+	},
+	history: function () {
+		return Histories.find({projectId: Session.get('currentClient')}, {sort: {'date': -1}}).fetch();
+	}
+});
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* Events
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+Template.clients.events({
+	'click .createClient': function (evt, template) {
+		var coll = $('.btn-group .active');
+		var products = [];
+		_.each(coll, function (item) {
+			products.push(item.value);
+		});
+		Clients.insert({
+			name: template.find('#name').value,
+			location: template.find('#location').value,
+			products: products,
+			// region: $('.btn-group .active').value,
+			description: template.find('#description').value,
+			createdBy: Meteor.userId(),
+			createdByEmail: getUserEmail(),
+			date: new Date,
+		});
+		template.find('#name').value = '';
+		template.find('#location').value = '';
+		template.find('#description').value = '';
+	},
+	'click .editClient': function (evt, template) {
+		var coll = $('.btn-group .active');
+		var products = [];
+		_.each(coll, function (item) {
+			products.push(item.value);
+		});
+		Clients.update(this._id, {
+			name: template.find('#name').value,
+			location: template.find('#location').value,
+			products: products,
+			description: template.find('#description').value,
+			createdBy: Meteor.userId(),
+			createdByEmail: getUserEmail(),
+			date: new Date,
+		});
+		Histories.insert({
+			clientId: this._id,
+			name: this.name,
+			location: this.location,
+			products: this.products,
+			description: this.description,
+			createdBy: this.createdBy,
+			createdByEmail: this.createdByEmail,
+			date: this.date,			
+		}); 
+	}, 
+	'click .card': function (evt, template) {
+		Session.set('currentClient', this._id);
+		Session.set('create', false);
+		Session.set('edit', true);
+	},
+	'click .deleteClient': function () {
+		Clients.remove({_id: this._id});
+		Session.set('create', true);
+		Session.set('edit', false);
+	}
+});
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* Rendered
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+Template.clients.rendered = function() {
+	$('#startDate').datepicker({
+		autoclose: true,
+	    todayHighlight: true,
+	    daysOfWeekDisabled: "0,6"
+	});
+	$('#endDate').datepicker({
+		autoclose: true,
+	    todayHighlight: true,
+	    daysOfWeekDisabled: "0,6"
+	});
+}
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* Handlebar Helpers
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+Handlebars.registerHelper("formatStandardDate", function(date) {
+	return moment(date).format("MM/DD/YYYY");
+});
+
+Handlebars.registerHelper("listHistory", function(id) {
+	return Histories.find({projectId: id}, {sort: {date: -1}}).fetch();
+});
+
+Handlebars.registerHelper("listService", function(id) {
+	return Services.find({projectId: id}, {sort: {date: -1}}).fetch();
+});
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* Functions
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+// Using their Meteor userId, find their email address
+var getUserEmail = function (item) {
+	if (Meteor.user()) {
+		var user = Meteor.user();
+		return user.emails[0].address;
+	} else {
+		return 'anon'
+	}
+};
